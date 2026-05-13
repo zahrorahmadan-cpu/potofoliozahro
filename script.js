fb31f796794aba6889a2e6a65e4e00bc90a8cef2
zahrorahmadan-cpu <zahrorahmadan@gmail.com>
2026-05-13 16:45:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('mobile-menu');
+
+btn.addEventListener('click', () => {
+ menu.classList.toggle('hidden');
+});
+
+
+var i = 0;
+var txt = "Welcome to My Portofolio";
+var speed = 80;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+      i++;
+    setTimeout(typeWriter, speed);
+    }
+  }
+typeWriter();
+
+
+const section = document.querySelector(".skill");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+
+      const bars = document.querySelectorAll(".progress-bar");
+
+      bars.forEach((bar, index) => {
+        const width = bar.getAttribute("data-width");
+
+        setTimeout(() => {
+          bar.style.width = width;
+        }, index * 250); // delay biar berurutan
+      });
+
+      observer.unobserve(section);
+    }
+  });
+}, {
+  threshold: 0.5
+});
+
+observer.observe(section);
+
